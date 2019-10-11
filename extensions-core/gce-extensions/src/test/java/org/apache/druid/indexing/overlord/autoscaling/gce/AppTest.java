b28766be4ef20a0af2718488ff9b8872ff6c525c
Giuseppe Martino <martinogiuseppe@gmail.com>
2019-10-11 11:10:11 +0200

    [WIP] WIP

---
+++ extensions-core/gce-extensions/src/test/java/org/apache/druid/indexing/overlord/autoscaling/gce/AppTest.java
@@ -0,0 +1,20 @@
+package org.apache.druid.indexing.overlord.autoscaling.gce;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
